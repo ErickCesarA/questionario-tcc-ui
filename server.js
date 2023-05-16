f337aae4a0629b5e1debed5a89cd1ed254b20dcb
ErickCesarA <erickcfalbuquerquef@gmail.com>
2023-05-16 10:52:58 -0300

    Angular-Heroku-v0.10

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const nomeApp = process.env.npm_package_name;
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/questionario-tcc-ui'));
+app.use(express.static(__dirname + '/dist/ui-questionario-tcc'));
 
 app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname + '/dist/questionario-tcc-ui/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/ui-questionario-tcc/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
